18c7bf3807ea602c24e0cae9829265fbbdef81fe
Fawzy9 <157676071+Fawzy9@users.noreply.github.com>
2024-09-08 11:40:16 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById('btn');
+
+
+onscroll = function(){
+if(scrollY>=400){
+    btn.style.display='block'
+}
+else{
+    btn.style.display='none'
+}
+
+}
+btn.onclick = function(){
+    scroll({
+     right:0,
+     top:0,
+     behavior:'smooth'
+
+    })
+}
